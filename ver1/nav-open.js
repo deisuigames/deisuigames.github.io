cf27b15e52fe9741992338c61238b8f5c5cb2a05
あっとまーく/atmark <atmark.admaster@gmail.com>
2023-12-29 17:29:00 +0900

    Delete ver1 directory

--- ver1/nav-open.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $('.click').click(function() {
-     $('.mainNav_wrap').toggleClass('active');
-    });
-    
-    $('.mainNav a[href^="#"]').click(function() {
-     $('.click').click();
-    });
-})
